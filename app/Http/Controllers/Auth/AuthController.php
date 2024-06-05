859c58a4cf8493a898e05843d2615944d57eb922
muhammedjafer <mohammadjafer990@gmail.com>
2024-06-05 10:45:21 +0300

    Finished authentication api

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,15 +3,16 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Models\User;
+use App\Enums\RoleEnum;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
-use App\Http\Requests\Auth\AuthRequest;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\SignUpRequest;
 
 class AuthController extends Controller
 {
-    public function login(AuthRequest $request)
+    public function login(LoginRequest $request)
     {
-
         $user = User::firstWhere('email', $request['email']);
 
         if ($user && Hash::check($request['password'], $user->password)) 
@@ -27,5 +28,29 @@ class AuthController extends Controller
                 'message' => 'failed'
             ], 400);
         }
+    }
+
+    public function signup(SignUpRequest $request)
+    {
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'role' => RoleEnum::PRODUCT_OWNER->value,
+            'email_verified_at' => now(),
+            'password' => Hash::make($request->password)
+        ]);
+
+        return response()->streamJson([
+            'message' => 'User signed up successfully'
+        ], 201);
+    }
+
+    public function logout()
+    {
+        auth('sanctum')->user()->tokens()->delete();
+
+        return response()->streamJson([
+            'message' => 'User logged out successfully'
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,11 @@ use App\Http\Controllers\User\UserController;
 Route::middleware('auth:sanctum')->group(function () {
 
     Route::resource('user', UserController::class)->only(['store', 'update', 'destroy'])->middleware('CheckRole:'.RoleEnum::PRODUCT_OWNER->value);
+    Route::post('logout', [AuthController::class, 'logout']);
 });
 
-Route::post('login', [AuthController::class, 'login'])->middleware('guest');
+Route::middleware('guest')->group(function () {
+
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('signup', [AuthController::class, 'signup']);
+});
